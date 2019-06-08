0bb073b2099bc6eb6a6ab2f200b02222f9f4445d
Abdullah Muhammed <abdo56668@gmail.com>
2019-06-08 13:50:16 +0200

    upload img name not binary

--- routes/web.php
+++ routes/web.php
@@ -19,34 +19,27 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/auth', function () {
-    //Perform a check whether the user is authenticated or not
-    //Remove the if block during production
-    if(!Auth::check())
-    {
-        //If they are not, we forcefully login the user with id=1
-        $user = App\User::find(1);
-        Auth::login($user);
-    }
-    return Auth::user();
-});
-
 Route::get('/get-profile-card', function(){
     $users = DB::table('users')
         ->join('profiles', 'users.id', '=', 'profiles.user_id')
         ->select('users.name', 'users.address', 'users.career',
         'profiles.hourlyRate', 'profiles.reviewRate')
         ->get();
-    // echo '<pre>'; print_r($users); echo '</pre>';
-    // var_dump($users);
     return $users;
 });
 
-// Route::group(['middleware' => ['web']], function() {
-// // Route::post('/api/register', 'UserController@store');
-// // Route::post('/api/register', 'UserController@store')->middleware('auth:web');
-// Route::post('/api/register', 'Auth\RegisterController@register');
-// Route::post('/api/login', 'Auth\LoginController@login');
-// // Route::post('/api/register', 'register')->middleware('auth:web');
-// });
+Route::get('/get-img/{imgName}', function($imgName){
+    $filePath = storage_path('app/public/'.$imgName);
+    if(!File::exists($filePath)){
+        abort(404);
+    }
+    $file = File::get($filePath);
+    $type = File::mimeType($filePath);
+    
+    $response = Response::make($file, 200);
+    $response->header("Content-type", $type);
 
+    return $response;
+});
+
+Route::resource('orders', 'UserController');
